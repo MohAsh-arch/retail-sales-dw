218d3e862c97359534b22f162792de18a3e0fbfb
MohAsh-Arch <moh.ash.arch.04@gmail.com>
2026-07-03 04:41:31 +0300

    found duplicate bug in branch table and fixed it by adding a checker

--- src/syn_gen.py
+++ src/syn_gen.py
@@ -2,7 +2,7 @@ import  psycopg2
 import os
 from dotenv import load_dotenv
 from psycopg2.extras import execute_values
-
+import random
 
 load_dotenv()
 
@@ -52,10 +52,21 @@ categories = [
 ]
 
 
-def insertion(conn , cursor, table_name, insert_column, values, pk_column):
+def empty_check(conn, cursor , table_name):
+    cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
+    count = cursor.fetchone()[0]
+    if count == 0:
+        return True
+    else: 
+        return False
+        
 
-    query = f"""INSERT INTO {table_name} ({insert_column}) VALUES %s
+def insertion(conn , cursor, table_name, insert_column, values, pk_column, on_conflict = True):
+    if on_conflict:
+        query = f"""INSERT INTO {table_name} ({insert_column}) VALUES %s
                     ON CONFLICT ({insert_column}) DO NOTHING"""
+    else:
+        query = f"""INSERT INTO {table_name} ({insert_column}) VALUES %s"""
     # saving by batches 
     execute_values(cursor, query, values)
 
@@ -68,28 +79,49 @@ def insertion(conn , cursor, table_name, insert_column, values, pk_column):
 regions_list = insertion(conn, cur, 'regions' , 'name' , regions , 'region_id')
 categories_list = insertion(conn , cur , 'category', 'category_name', categories , 'category_id')
 
-print(regions_list)
-print(categories_list)
+#---
+#--- creating the branch table ---
+
+cur.execute('SELECT name , region_id FROM regions')
+regions_dict = dict(cur.fetchall())
+
+region_city_map = {
+    "Andes": ["Bogotá", "Cusco"],
+    "Alps": ["Geneva", "Innsbruck"],
+    "Sahara": ["Cairo", "Khartoum"],
+    "Gobi": ["Ulaanbaatar", "Hohhot"],
+    "Crimea": ["Sevastopol", "Yalta"],
+    "Punjab": ["Lahore", "Amritsar"],
+    "Balkans": ["Belgrade", "Sofia"],
+    "Siberia": ["Novosibirsk", "Irkutsk"],
+    "Baltics": ["Riga", "Tallinn"],
+    "Levant": ["Beirut", "Amman"]
+}
+
+
+if empty_check(conn,cur,'branch'):
+    region_city_id_map = dict()
+    for key , value in regions_dict.items() : 
+        region_city_id_map[value] = region_city_map[key] 
+
+
+    branch_insert_tuples = []
+
+    for id , cities in region_city_id_map.items():
+        no_of_branches = random.randint(3,5)
+        for i in range(no_of_branches):
+            city = random.choice(cities)
+            branch_insert_tuples.append((city,id))
+
+
+
+    insertion(conn, cur, 'branch' , 'city , region_id' , branch_insert_tuples , 'branch_id', on_conflict=False)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(empty_check(conn, cur, 'branch'))
 
 
 
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+region_city_id_map = dict()
+# for key , value in regions_dict.items() : 
+#     region_city_id_map[value] = region_city_map[key] 
+
+
+# branch_insert_tuples = []
+
+# for id , cities in region_city_id_map.items():
+#     no_of_branches = random.randint(3,5)
+#     for i in range(no_of_branches):
+#         city = random.choice(cities)
+#         branch_insert_tuples.append((city,id))
