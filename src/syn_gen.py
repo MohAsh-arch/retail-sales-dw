4c34ffe24370a57a4dcfb75f18a9273da2d261bb
MohAsh-Arch <moh.ash.arch.04@gmail.com>
2026-07-04 05:26:29 +0300

    finishing phase 1 and document it

--- src/syn_gen.py
+++ src/syn_gen.py
@@ -5,6 +5,7 @@ from psycopg2.extras import execute_values
 import random
 from faker import Faker
 from faker_commerce import Provider
+from collections import defaultdict
 
 
 
@@ -29,7 +30,11 @@ except Exception as e:
 cur = conn.cursor()
 
 
-
+def get_list_of_column(cur,table_name ,column_name):
+    cur.execute(f'SELECT {column_name} FROM {table_name}')
+    tuples = cur.fetchall()
+    list_of_column = [id[0] for id in tuples]
+    return list_of_column
 
 
 def empty_check(conn, cursor , table_name):
@@ -41,7 +46,7 @@ def empty_check(conn, cursor , table_name):
         return False
         
 
-def insertion(conn , cursor, table_name, insert_column, values, pk_column, on_conflict = True,conflict_column=''):
+def insertion(conn , cursor, table_name, insert_column, values, on_conflict = True,conflict_column=''):
     if on_conflict:
         query = f"""INSERT INTO {table_name} ({insert_column}) VALUES %s
                     ON CONFLICT ({conflict_column}) DO NOTHING"""
@@ -50,11 +55,6 @@ def insertion(conn , cursor, table_name, insert_column, values, pk_column, on_co
     # saving by batches 
     execute_values(cursor, query, values)
 
-    cursor.execute(f"SELECT {pk_column} FROM {table_name}")
-    result_tuple = cursor.fetchall()
-    result_list = [row[0] for row in result_tuple]
-
-    return result_list
 
 
 #--- region insertion ---
@@ -72,7 +72,7 @@ regions = [
 ]
 
 if empty_check(conn, cur, 'regions'):
-    regions_list = insertion(conn, cur, 'regions' , 'name' , regions , 'region_id', on_conflict=True ,conflict_column="name")
+    insertion(conn, cur, 'regions' , 'name' , regions ,  on_conflict=True ,conflict_column="name")
 
 #--- category insertion ---
 categories = [
@@ -91,7 +91,7 @@ categories = [
 ]
 
 if empty_check(conn, cur, 'category'):
-    categories_list = insertion(conn , cur , 'category', 'category_name', categories , 'category_id', on_conflict=True,conflict_column='category_name' )
+    insertion(conn , cur , 'category', 'category_name', categories ,on_conflict=True,conflict_column='category_name' )
 
 #--- creating the branch table ---
 
@@ -128,15 +128,13 @@ if empty_check(conn,cur,'branch'):
 
 
 
-    insertion(conn, cur, 'branch' , 'city , region_id' , branch_insert_tuples , 'branch_id', on_conflict=False)
+    insertion(conn, cur, 'branch' , 'city , region_id' , branch_insert_tuples ,on_conflict=False)
 
 fake = Faker()
 #--- product insertion ---
 fake.add_provider(Provider)
 product_insert_tuples = []
-cur.execute('SELECT category_id FROM category')
-categ_id_tuples = cur.fetchall()
-categ_id_list = [id[0] for id in categ_id_tuples]
+categ_id_list = get_list_of_column(cur,'category','category_id')
 
 
 if empty_check(conn, cur, 'product'):
@@ -146,7 +144,7 @@ if empty_check(conn, cur, 'product'):
         prod_price = round(random.uniform(5, 2000),2)
         product_insert_tuples.append((prod_categ,prod_name,prod_price))
 
-    insertion(conn , cur , 'product', 'category_id , name , price' , product_insert_tuples , 'product_id',on_conflict=True,conflict_column='name')
+    insertion(conn , cur , 'product', 'category_id , name , price' , product_insert_tuples ,on_conflict=True,conflict_column='name')
 
 #--- customer insertion ---
 
@@ -160,16 +158,14 @@ if empty_check(conn,cur,'customer'):
         address = fake.address()
         customer_insert_tuples.append((first_name,last_name,phone,email,address))
     customer_insert_column = 'first_name , last_name, phone, email, address'
-    insertion(conn ,cur, 'customer', customer_insert_column , customer_insert_tuples, 'customer_id', on_conflict=True, conflict_column='email' )
+    insertion(conn ,cur, 'customer', customer_insert_column , customer_insert_tuples, on_conflict=True, conflict_column='email' )
 
 
 #--- employee insertion ---
 
 if empty_check(conn,cur,'employee'): 
     employee_insert_tuples = []
-    cur.execute('SELECT branch_id FROM branch')
-    branch_id_tuples = cur.fetchall()
-    branch_id_list = [id[0] for id in branch_id_tuples]
+    branch_id_list = get_list_of_column(cur,'branch','branch_id')
 
     for id in branch_id_list:
         no_of_emp = random.randint(15,20)
@@ -181,7 +177,56 @@ if empty_check(conn,cur,'employee'):
             employee_insert_tuples.append((first_name,last_name,email,branch_emp_id))
 
     employee_insert_column = 'first_name,last_name,email,branch_id'
-    insertion(conn ,cur, 'employee', employee_insert_column , employee_insert_tuples, 'employee_id', on_conflict=True, conflict_column='email' )
+    insertion(conn ,cur, 'employee', employee_insert_column , employee_insert_tuples, on_conflict=True, conflict_column='email' )
+
+#--- orders insertion --- 
+if empty_check(conn,cur,'orders'):
+    branch_id_list = get_list_of_column(cur,'branch','branch_id')
+
+    customer_id_list = get_list_of_column(cur,'customer','customer_id')
+
+    cur.execute('SELECT branch_id , employee_id FROM employee')
+    list_of_branch_emp_tuples = cur.fetchall()
+    dict_branch_emp_id = dict()
+
+    for key, value in list_of_branch_emp_tuples:
+        if key not in dict_branch_emp_id:
+            dict_branch_emp_id[key] = []
+        dict_branch_emp_id[key].append(value)
+
+    orders_insert_tuples = []
+    for i in range(75000):
+        orders_branch_id = random.choice(branch_id_list)
+        orders_employee_id = random.choice(dict_branch_emp_id[orders_branch_id])
+        order_timestamp = fake.date_time_between( start_date='-1y',end_date='now')
+        orders_customer = random.choice(customer_id_list)
+        orders_insert_tuples.append((orders_customer,order_timestamp,orders_branch_id,orders_employee_id))
+    
+    orders_insertion_columns = 'customer_id , order_timestamp , branch_id , employee_id'
+    insertion(conn,cur,'orders',orders_insertion_columns, orders_insert_tuples , on_conflict=False)
+
+#--- orders insertion --- 
+if empty_check(conn,cur, 'order_items'):
+    items_list_tuples = []
+    order_id_list = get_list_of_column(cur,'orders' , 'order_id')
+    product_id_list = get_list_of_column(cur,'product' , 'product_id')
+    cur.execute('SELECT product_id, price FROM product')
+    list_of_prod_price_tuples = cur.fetchall()
+    product_id_price_dict = dict(list_of_prod_price_tuples)
+
+
+
+    for order_id in order_id_list:
+        items_per_order = random.randint(1,5)
+        for i in range(items_per_order):
+            item_product_id = random.choice(product_id_list)
+            item_quantity = random.randint(1,10)
+            item_unit_price = product_id_price_dict[item_product_id]
+            items_list_tuples.append((order_id,item_product_id,item_quantity,item_unit_price))
+    items_insertion_column = 'order_id,product_id,quantity,unit_price'
+    insertion(conn,cur,'order_items',items_insertion_column, items_list_tuples , on_conflict=True,conflict_column='order_id, product_id')
+
+
 
 
 
